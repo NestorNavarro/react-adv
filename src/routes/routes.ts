32d25bcf4e352edc2c4a608fda5de16153233e10
NestorNavarro <nestor.navarroarizga@gmail.com>
2022-03-21 12:26:29 -0600

    Basic LazyLoad configuration

---
+++ src/routes/routes.ts
@@ -0,0 +1,35 @@
+import { lazy } from 'react';
+
+type JSXComponent =  () => JSX.Element;
+
+interface Route {
+    to   : string,
+    path : string,
+    name : string,
+    Component : React.LazyExoticComponent<JSXComponent> | JSXComponent,
+};
+
+const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */  "../01-lazyload/Pages/LazyPage1"));
+const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */  "../01-lazyload/Pages/LazyPage2"));
+const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */  "../01-lazyload/Pages/LazyPage3"));
+
+export const routes: Route[] = [
+    {
+        to : "/lazy1",
+        path : "lazy1",
+        Component : Lazy1,
+        name : "Lazy 1",
+    },
+    {
+        to : "/lazy2",
+        path : "lazy2",
+        Component : Lazy2,
+        name : "Lazy 2",
+    },
+    {
+        to : "/lazy3",
+        path : "lazy3",
+        Component : Lazy3,
+        name : "Lazy 3",
+    },
+];
